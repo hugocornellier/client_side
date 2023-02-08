edef67a86c5bd8b222eb6a3e8f7ac4571dc634d9
hugocornellier <hugo.cornellier@gmail.com>
2023-02-08 19:45:07 -0400

    a

--- client.c
+++ client.c
@@ -6,17 +6,17 @@
 
 int main() {
     int clientSocket;
-    char buffer[1024];
+    char buffer[2048];
     struct sockaddr_in serverAddr;
     socklen_t addr_size;
     clientSocket = socket(PF_INET, SOCK_STREAM, 0);
     serverAddr.sin_family = AF_INET;
-    serverAddr.sin_port = htons(7891);
+    serverAddr.sin_port = htons(7878);
     serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
     memset(serverAddr.sin_zero, '\0', sizeof serverAddr.sin_zero);
     addr_size = sizeof serverAddr;
     connect(clientSocket, (struct sockaddr *) &serverAddr, addr_size);
-    recv(clientSocket, buffer, 1024, 0);
-    printf("Data received: %s",buffer);
+    strcpy(buffer,"HugoCornellier_3680298_A1\n");
+    send(clientSocket,buffer,64,0);
     return 0;
 }
--- server.c
+++ server.c
@@ -5,13 +5,13 @@
 
 int main() {
     int welcomeSocket, newSocket;
-    char buffer[1024];
+    char buffer[2048];
     struct sockaddr_in serverAddr;
     struct sockaddr_storage serverStorage;
     socklen_t addr_size;
     welcomeSocket = socket(PF_INET, SOCK_STREAM, 0);
     serverAddr.sin_family = AF_INET;
-    serverAddr.sin_port = htons(7891);
+    serverAddr.sin_port = htons(7878);
     serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
     memset(serverAddr.sin_zero, '\0', sizeof serverAddr.sin_zero);
     bind(welcomeSocket, (struct sockaddr *) &serverAddr, sizeof(serverAddr));
@@ -21,7 +21,8 @@ int main() {
         printf("Error\n");
     addr_size = sizeof serverStorage;
     newSocket = accept(welcomeSocket, (struct sockaddr *) &serverStorage, &addr_size);
-    strcpy(buffer,"Hello World\n");
-    send(newSocket,buffer,13,0);
+    printf("A client has connected!\n");
+    recv(newSocket, buffer, 1024, 0);
+    printf("Data received: %s",buffer);
     return 0;
 }
